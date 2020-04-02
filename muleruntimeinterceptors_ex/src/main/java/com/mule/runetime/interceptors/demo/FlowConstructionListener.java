f99977bef4de75dc4eb260cfeac0e399a806c33e
Amr Salah <amr.salah@mulesoft.com>
2020-04-03 12:00:05 +1300

    Add Postman collection and exception notification capturing

--- muleruntimeinterceptors_ex/src/main/java/com/mule/runetime/interceptors/demo/FlowConstructionListener.java
+++ muleruntimeinterceptors_ex/src/main/java/com/mule/runetime/interceptors/demo/FlowConstructionListener.java
@@ -3,12 +3,19 @@ package com.mule.runetime.interceptors.demo;
 import org.mule.runtime.api.notification.FlowConstructNotification;
 import org.mule.runtime.api.notification.FlowConstructNotificationListener;
 import org.mule.runtime.api.notification.Notification;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 
 public class FlowConstructionListener implements FlowConstructNotificationListener<FlowConstructNotification> {
 
+    private static Logger LOGGER = LoggerFactory.getLogger(FlowConstructionListener.class);
+
 	@Override
 	public void onNotification(FlowConstructNotification notification) {
-		System.out.println("FlowConstructNotification  :: Step :: ==> " +  notification)	;
+		//Use System.out
+		//System.out.println("FlowConstructNotification  :: Step :: ==> " +  notification)	;
+		// or use logger framework to log the required details
+		LOGGER.info("FlowConstructNotification  :: Step :: ==> " + notification);
 		
 	}
 
